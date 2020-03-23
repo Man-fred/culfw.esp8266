4cc3b22a280bc50a11790d73867d695f3b444fe5
Manfred <Man-fred@users.noreply.github.com>
2020-03-23 20:12:44 +0100

    ignore private.h in commits

--- libraries/private/private.h
+++ libraries/private/private.h
@@ -1,8 +1,8 @@
 #ifndef _PRIVATE_H
 #define _PRIVATE_H
 
-#define STASSID "your-ssid"
-#define STAPSK  "your-password"
+#define STASSID "your-ssid1"
+#define STAPSK  "your-password1"
 
 
 #endif // _PRIVATE_H
