593f462d65c3720162b3dfaf0129fb923cfb5081
Memphiz <memphis@machzwo.de>
2020-10-01 22:35:40 +0200

    EEPROM: - allow ssid with up to 31 chars and wpakey with up to 64 bytes

--- libraries/fncollection/fncollection.h
+++ libraries/fncollection/fncollection.h
@@ -72,12 +72,16 @@ extern FNCOLLECTIONClass FNcol;
 # define EE_IP4_NTPOFFSET    (EE_IP4_TCPLINK_PORT+2)
 # define EE_STR_LEN        20
 # ifdef ESP8266
+#   define EE_MAX_ENTRY_LEN  64
+#   define EE_WPA_KEY_LEN    EE_MAX_ENTRY_LEN
+#   define EE_SSID_LEN       32
 #   define EE_WPA_SSID       (EE_IP4_NTPOFFSET+1)
-#   define EE_WPA_KEY        (EE_WPA_SSID+EE_STR_LEN)
-#   define EE_NAME           (EE_WPA_KEY+EE_STR_LEN)
+#   define EE_WPA_KEY        (EE_WPA_SSID+EE_SSID_LEN)
+#   define EE_NAME           (EE_WPA_KEY+EE_WPA_KEY_LEN)
 #   define EE_OTA_SERVER     (EE_NAME+EE_STR_LEN)
 #   define EE_ETH_LAST       (EE_OTA_SERVER+4)
 # else
+#   define EE_MAX_ENTRY_LEN    EE_STR_LEN
 #   define EE_ETH_LAST       (EE_IP4_NTPOFFSET+1)
 # endif
 #else
